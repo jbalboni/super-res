d85547294f954101ec3b02fc830cceaf85ca9da7
Jeff Balboni <jbalboni@gmail.com>
2015-08-31 23:47:25 -0400

    Actually publish dists

--- dist/super-res.js
+++ dist/super-res.js
@@ -3,15 +3,17 @@ var _createClass = (function () { function defineProperties(target, props) { for
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
 
 (function (global, factory) {
-  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory(require('q'), require('superagent'), require('route-parser')) : typeof define === 'function' && define.amd ? define(['q', 'superagent', 'route-parser'], factory) : global.superRes = factory(global.Q, global.superagent, global.Route);
-})(this, function (Q, superagent, Route) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory(require('q'), require('superagent'), require('route-parser'), require('cache-manager')) : typeof define === 'function' && define.amd ? define(['q', 'superagent', 'route-parser', 'cache-manager'], factory) : global.superRes = factory(global.Q, global.superagent, global.Route, global.cacheManager);
+})(this, function (Q, superagent, Route, cacheManager) {
   'use strict';
 
   var actionDefaults = actionDefaults = {
     method: 'GET',
     responseType: 'json',
     transformRequest: [],
-    transformResponse: []
+    transformResponse: [],
+    withCredentials: false,
+    cache: null
   };
 
   var ResourceAction_js__assign = ResourceAction_js__assign || require('object.assign');
@@ -35,9 +37,18 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
       this.config = ResourceAction_js__assign({ url: url }, actionDefaults, action);
       this.route = new Route(this.config.url);
       this.defaultParams = defaultParams;
+
+      if (this.config.cache === true) {
+        this.config.cache = cacheManager.caching({ store: 'memory', max: 100, ttl: 1200 });
+      }
     }
 
     _createClass(ResourceAction, [{
+      key: 'getCacheKey',
+      value: function getCacheKey(params, data) {
+        return this.route.reverse(params) + JSON.stringify(data);
+      }
+    }, {
       key: 'buildRequest',
       value: function buildRequest(params, data) {
         var method = this.config.method.toLowerCase();
@@ -54,10 +65,17 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
           currentRequest.clearTimeout();
         }
 
-        if (data && method === 'get') {
-          currentRequest = currentRequest.query(applyRequestTransforms(this.config.transformRequest, currentRequest, data));
-        } else if (data) {
-          currentRequest = currentRequest.send(applyRequestTransforms(this.config.transformRequest, currentRequest, data));
+        if (this.config.withCredentials) {
+          currentRequest = currentRequest.withCredentials();
+        }
+
+        if (data) {
+          var transformedData = applyRequestTransforms(this.config.transformRequest, currentRequest, data);
+          if (method === 'get') {
+            currentRequest = currentRequest.query(transformedData);
+          } else {
+            currentRequest = currentRequest.send(transformedData);
+          }
         }
 
         return currentRequest;
@@ -68,16 +86,39 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
         var _this = this;
 
         var deferred = Q.defer();
+        var fullParams = ResourceAction_js__assign({}, this.defaultParams, params);
 
-        console.log(superagent);
-        this.buildRequest(ResourceAction_js__assign({}, this.defaultParams, params), data).end(function (err, res) {
-          console.log('here');
-          if (err) {
-            deferred.reject(err);
-          } else {
-            deferred.resolve(applyResponseTransforms(_this.config.transformResponse, res));
-          }
-        });
+        var doRequest = function doRequest() {
+          _this.buildRequest(fullParams, data).end(function (err, res) {
+            if (err) {
+              deferred.reject(err);
+            } else {
+              var transformedReponse = applyResponseTransforms(_this.config.transformResponse, res);
+
+              if (_this.config.cache) {
+                _this.config.cache.set(_this.getCacheKey(fullParams, data), transformedReponse);
+              }
+
+              deferred.resolve(transformedReponse);
+            }
+          });
+        };
+
+        if (this.config.cache && this.config.method.toLowerCase() === 'get') {
+          var key = this.getCacheKey(fullParams, data);
+          this.config.cache.get(key, function (err, result) {
+            if (err) {
+              deferred.reject(err);
+            } else if (result) {
+              deferred.resolve(result);
+            } else {
+              doRequest();
+            }
+          });
+        } else {
+          doRequest();
+        }
+
         return deferred.promise;
       }
     }]);
@@ -119,7 +160,7 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
     return resource;
   };
 
-  super_res__superRes.promiseWrapper = function promiseWrapper(promiseObj) {
+  super_res__superRes.promiseWrapper = function promiseWrapper(wrapperFunc) {
     return function (resource) {
       var proxiedResource = super_res__assign({}, resource);
       Object.getOwnPropertyNames(resource).forEach(function (name) {
@@ -130,8 +171,7 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
           }
 
           var promise = actionFunction.apply(proxiedResource, args);
-          //TODO am I swallowing rejected promises here?
-          return promiseObj.when(promise);
+          return wrapperFunc(promise);
         };
       });
       return proxiedResource;
